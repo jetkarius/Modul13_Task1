d715acc17a3cbe42e5cacbe8ef7c627c35684170
boroksid@yandex.ru <boroksid@yandex.ru>
2023-02-19 22:03:20 +0300

    Добавьте файлы проекта.

---
+++ Modul13_Task1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Modul_13_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("H:\\Text1.txt");
+            CreateListT(text);
+
+            CreateLinkList(text);
+        }
+        static void CreateListT(string text)
+        {
+
+            Console.WriteLine("\nCreateListT начал работу");
+
+            char[] delimiters = new char[] { ' ', '\r', '\n', ',', '.' };
+
+            var wordslist = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var newElements = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var timer = new Stopwatch();
+            timer.Start();
+
+            wordslist.AddRange(newElements);
+
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            Console.WriteLine("CreateListT завершил работу");
+            Console.ReadKey();
+
+
+        }
+        static void CreateLinkList(string text)
+        {
+            Console.WriteLine("\nCreateLinkList начал работу");
+
+            char[] delimiters = new char[] { ' ', '\r', '\n', ',', '.' };
+
+            var wordslist = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var newElements = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var linkedList = new LinkedList<string>(wordslist);
+
+            var timer = new Stopwatch();
+            timer.Start();
+
+            foreach (var el in newElements)
+            {
+                linkedList.AddAfter(linkedList.First, el);
+            }
+
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            Console.WriteLine("CreateLinkList завершил работу");
+            Console.ReadKey();
+        }
+    }
+}
